f2d085248cc115f11f7e50bfdecd0bacfb77a355
zhougp <guangping.zhou@outlook.com>
2023-12-11 16:07:21 +0800

    car light

---
+++ carlight/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using carlight.Services;
+using carlight.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace carlight
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ carlight/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using carlight.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace carlight.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
